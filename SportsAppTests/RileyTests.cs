716090c6006a2dfcd825863ba3704d7c953aab64
rbadnin <rbadnin@calpoly.edu>
2022-12-05 17:30:48 +0100

    added fetch account tests

--- SportsAppTests/RileyTests.cs
+++ SportsAppTests/RileyTests.cs
@@ -2,28 +2,50 @@ using SportsAppControllers;
 
 namespace SportsAppTests
 {
+    // This class tests fetchAccount()
     [TestClass]
     public class RileyTests
     {
+        UserDatabaseManagement userDatabaseManagement = new UserDatabaseManagement();
+
+        // Tests a valid citizen user fetch
         [TestMethod]
         public void TestMethod1()
         {
-
+            string username = "anisha";
+            string password = "anisha";
+            var result = userDatabaseManagement.fetchAccount(username, password).Result;
+            Assert.IsTrue(result.Username == username && result.Password == password);
         }
 
+        // Tests a valid admin user fetch
         [TestMethod]
         public void TestMethod2()
         {
+            string username = "admin";
+            string password = "admin1234";
+            var result = userDatabaseManagement.fetchAccount(username, password).Result;
+            Assert.IsTrue(result.Username == username && result.Password == password);
         }
 
+        // Tests a bad username/password fetch
         [TestMethod]
         public void TestMethod3()
         {
+            string username = "badusername";
+            string password = "admin1234";
+            var result = userDatabaseManagement.fetchAccount(username, password).Result;
+            Assert.IsNull(result);
         }
 
+        // Tests a bad password fetch
         [TestMethod]
         public void TestMethod4()
         {
+            string username = "admin";
+            string password = "badpassword";
+            var result = userDatabaseManagement.fetchAccount(username, password).Result;
+            Assert.IsNull(result);
         }
     }
 }
